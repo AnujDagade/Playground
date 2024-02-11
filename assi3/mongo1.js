4d885c52c8402983599f955eabe1671e286d7f0d
Anuj Dagade <dagadeanuj@gmail.com>
2024-02-11 09:45:18 +0000

    Refactor MongoDB connection and retrieval code

--- assi3/mongo1.js
+++ assi3/mongo1.js
@@ -1,40 +1,36 @@
 const MongoClient = require('mongodb').MongoClient;
 
 // Connection URL
-const url = 'mongodb://localhost:27017';
+//const url = 'mongodb://localhost:27017';
+
 // Connection URL
 //const url = 'mongodb://username:password@localhost:27017';
 // Database Name
-const dbName = 'mydatabase';
+const foo = async () => {
+    let users = []
+  
+    const URI = url
+   
+    const client = new MongoClient(
+      URI
+    )
+    let conn = null
 
-// Create a new MongoClient
-const client = new MongoClient(url);
-
-// Use connect method to connect to the Server
-client.connect(function(err) {
-    if (err) {
-        console.error('Error connecting to MongoDB:', err);
-        return;
+    try {
+      client.connect()
+      conn = client.db("Movie")
+      const ngo = conn.collection("Film")
+      const result = ngo.find()
+      for await (doc of result) {
+        users.push(doc)
+        console.log(users);
+      }
+    } catch (error) {
+      console.error(error)
     }
+    finally{
+      client.close()
+    }
+};
 
-    console.log('Connected successfully to MongoDB');
-
-    const db = client.db(dbName);
-
-    // Get the customers collection
-    const collection = db.collection('customers');
-
-    // Find all documents in the customers collection
-    collection.find({}).toArray(function(err, result) {
-        if (err) {
-            console.error('Error retrieving documents:', err);
-            return;
-        }
-
-        console.log('All records from the "customers" table:');
-        console.log(result);
-    });
-
-    // Close the connection
-    client.close();
-});
+foo();
