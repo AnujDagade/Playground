c3a1dc6d65d81e3ac517bc8a49713fd9a19ea4ac
Anuj Dagade <dagadeanuj@gmail.com>
2024-09-02 16:57:44 +0000

    chore: Add login and welcome routes to app.routes.ts

---
+++ FullStack/AngularJsI/angularjs-welcome-app/welcome/src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  imports: [ReactiveFormsModule]
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Placeholder for actual login logic, like calling a service
+      const { username, password } = this.loginForm.value;
+
+      if (username === 'user' && password === 'password') {
+        this.router.navigate(['/welcome']);
+      } else {
+        alert('Invalid credentials');
+      }
+    }
+  }
+}
