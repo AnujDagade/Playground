56dc79a7f7b6d096c5caa299718e1bbb48912d45
Anuj Dagade <dagadeanuj@gmail.com>
2024-09-02 15:36:57 +0000

    feat: Add standalone property to alert component

--- FullStack/AngularJsI/angularjs-welcome-app/welcome/src/app/alert/alert.component.ts
+++ FullStack/AngularJsI/angularjs-welcome-app/welcome/src/app/alert/alert.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-alert',
+  standalone: true,
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.css']
 })
