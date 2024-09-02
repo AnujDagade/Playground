2662202978c5a3ca44d53c99fb156b85e76136f0
Anuj Dagade <dagadeanuj@gmail.com>
2024-09-02 16:43:23 +0000

    feat: Add functionality to add two numbers in app component

--- FullStack/AngularJsI/angularjs-welcome-app/welcome/src/app/app.component.ts
+++ FullStack/AngularJsI/angularjs-welcome-app/welcome/src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AlertComponent } from './alert/alert.component';
+import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AlertComponent],
+  imports: [RouterOutlet, AlertComponent,NgIf,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'welcome';
+  num1: number = 0;
+  num2: number = 0;
+  result: number = 0;
+  showResult: boolean = false;
+
+  addNumbers() {
+    this.result = this.num1 + this.num2;
+    this.showResult = true;
+  }
 }
