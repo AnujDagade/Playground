118c18591461a3b5c0936367a2f4589ad26bd4b2
Anuj Dagade <dagadeanuj@gmail.com>
2024-09-08 08:26:07 +0000

    feat: Add AddComponent to app.module.ts and app-routing.module.ts

--- FullStack/Angular/AnujDagade/src/app/app.module.ts
+++ FullStack/Angular/AnujDagade/src/app/app.module.ts
@@ -5,15 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MessageComponent } from './message/message.component';
 import { AlertComponent } from './alert/alert.component';
+import { AddComponent } from './add/add.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     MessageComponent,
-    AlertComponent
+    AlertComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
