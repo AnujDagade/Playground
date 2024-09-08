a7084b16f3c560c9e57d34594cf3de926bbf1ce2
Anuj Dagade <dagadeanuj@gmail.com>
2024-09-08 08:10:33 +0000

    chore: Update app.component.html and app.routes.ts

---
+++ FullStack/Angular/AnujDagade/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MessageComponent } from './message/message.component';
+import { AlertComponent } from './alert/alert.component';
+// import { AddComponent } from './add/add.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/msg', pathMatch: 'full' },
+  { path: 'msg', component: MessageComponent },
+  { path: 'alert', component: AlertComponent },
+  // { path: 'add', component: AddComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
