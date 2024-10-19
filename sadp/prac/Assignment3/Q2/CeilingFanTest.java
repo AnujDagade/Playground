64d312daff2322b66116e83e3f32fa7ddabeea7f
Anuj Dagade <dagadeanuj@gmail.com>
2024-10-19 13:35:01 +0530

    Add files via upload

---
+++ sadp/prac/Assignment3/Q2/CeilingFanTest.java
@@ -0,0 +1,210 @@
+// Command interface
+interface Command {
+    void execute();
+    void undo();
+}
+
+// CeilingFan class
+class CeilingFan {
+    static final int HIGH = 3;
+    static final int MEDIUM = 2;
+    static final int LOW = 1;
+    static final int OFF = 0;
+
+    private String location;
+    private int speed;
+
+    public CeilingFan(String location) {
+        this.location = location;
+        speed = OFF;
+    }
+
+    public void high() {
+        speed = HIGH;
+        System.out.println(location + " Ceiling Fan is on HIGH");
+    }
+
+    public void medium() {
+        speed = MEDIUM;
+        System.out.println(location + " Ceiling Fan is on MEDIUM");
+    }
+
+    public void low() {
+        speed = LOW;
+        System.out.println(location + " Ceiling Fan is on LOW");
+    }
+
+    public void off() {
+        speed = OFF;
+        System.out.println(location + " Ceiling Fan is OFF");
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+}
+
+// CeilingFanCommand classes
+class CeilingFanHighCommand implements Command {
+    CeilingFan ceilingFan;
+    int prevSpeed;
+
+    public CeilingFanHighCommand(CeilingFan ceilingFan) {
+        this.ceilingFan = ceilingFan;
+    }
+
+    @Override
+    public void execute() {
+        prevSpeed = ceilingFan.getSpeed();
+        ceilingFan.high();
+    }
+
+    @Override
+    public void undo() {
+        if (prevSpeed == CeilingFan.HIGH) {
+            ceilingFan.high();
+        } else if (prevSpeed == CeilingFan.MEDIUM) {
+            ceilingFan.medium();
+        } else if (prevSpeed == CeilingFan.LOW) {
+            ceilingFan.low();
+        } else if (prevSpeed == CeilingFan.OFF) {
+            ceilingFan.off();
+        }
+    }
+}
+
+class CeilingFanMediumCommand implements Command {
+    CeilingFan ceilingFan;
+    int prevSpeed;
+
+    public CeilingFanMediumCommand(CeilingFan ceilingFan) {
+        this.ceilingFan = ceilingFan;
+    }
+
+    @Override
+    public void execute() {
+        prevSpeed = ceilingFan.getSpeed();
+        ceilingFan.medium();
+    }
+
+    @Override
+    public void undo() {
+        if (prevSpeed == CeilingFan.HIGH) {
+            ceilingFan.high();
+        } else if (prevSpeed == CeilingFan.MEDIUM) {
+            ceilingFan.medium();
+        } else if (prevSpeed == CeilingFan.LOW) {
+            ceilingFan.low();
+        } else if (prevSpeed == CeilingFan.OFF) {
+            ceilingFan.off();
+        }
+    }
+}
+
+class CeilingFanLowCommand implements Command {
+    CeilingFan ceilingFan;
+    int prevSpeed;
+
+    public CeilingFanLowCommand(CeilingFan ceilingFan) {
+        this.ceilingFan = ceilingFan;
+    }
+
+    @Override
+    public void execute() {
+        prevSpeed = ceilingFan.getSpeed();
+        ceilingFan.low();
+    }
+
+    @Override
+    public void undo() {
+        if (prevSpeed == CeilingFan.HIGH) {
+            ceilingFan.high();
+        } else if (prevSpeed == CeilingFan.MEDIUM) {
+            ceilingFan.medium();
+        } else if (prevSpeed == CeilingFan.LOW) {
+            ceilingFan.low();
+        } else if (prevSpeed == CeilingFan.OFF) {
+            ceilingFan.off();
+        }
+    }
+}
+
+class CeilingFanOffCommand implements Command {
+    CeilingFan ceilingFan;
+    int prevSpeed;
+
+    public CeilingFanOffCommand(CeilingFan ceilingFan) {
+        this.ceilingFan = ceilingFan;
+    }
+
+    @Override
+    public void execute() {
+        prevSpeed = ceilingFan.getSpeed();
+        ceilingFan.off();
+    }
+
+    @Override
+    public void undo() {
+        if (prevSpeed == CeilingFan.HIGH) {
+            ceilingFan.high();
+        } else if (prevSpeed == CeilingFan.MEDIUM) {
+            ceilingFan.medium();
+        } else if (prevSpeed == CeilingFan.LOW) {
+            ceilingFan.low();
+        } else if (prevSpeed == CeilingFan.OFF) {
+            ceilingFan.off();
+        }
+    }
+}
+
+// RemoteControl class
+class RemoteControl {
+    Command command;
+    Command undoCommand;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void pressButton() {
+        command.execute();
+        undoCommand = command;
+    }
+
+    public void pressUndo() {
+        undoCommand.undo();
+    }
+}
+
+// Main class
+public class CeilingFanTest {
+    public static void main(String[] args) {
+        RemoteControl remote = new RemoteControl();
+
+        CeilingFan ceilingFan = new CeilingFan("Living Room");
+
+        CeilingFanHighCommand ceilingFanHigh = new CeilingFanHighCommand(ceilingFan);
+        CeilingFanMediumCommand ceilingFanMedium = new CeilingFanMediumCommand(ceilingFan);
+        CeilingFanLowCommand ceilingFanLow = new CeilingFanLowCommand(ceilingFan);
+        CeilingFanOffCommand ceilingFanOff = new CeilingFanOffCommand(ceilingFan);
+
+        // Turn the ceiling fan on to high
+        remote.setCommand(ceilingFanHigh);
+        remote.pressButton();
+
+        // Turn the ceiling fan on to medium
+        remote.setCommand(ceilingFanMedium);
+        remote.pressButton();
+
+        // Undo the previous command
+        remote.pressUndo();
+
+        // Turn the ceiling fan off
+        remote.setCommand(ceilingFanOff);
+        remote.pressButton();
+
+
+        // Undo the previous command
+        remote.pressUndo();
+    }
+}
